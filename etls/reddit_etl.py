a43d9db64a03fdb9fb4570f91e5c4861d3549ef8
arthurbrito <arthurbrito@alu.ufc.br>
2024-11-14 10:07:22 -0300

    adjusting reddit api consumption

--- etls/reddit_etl.py
+++ etls/reddit_etl.py
@@ -7,14 +7,20 @@ from utils.constants import POST_FIELDS
 
 def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
     try:
-        reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,user_agent=user_agent, username='Glad-Speed8358', password='!Aawwodtr1964')
+        reddit = praw.Reddit(
+            client_id=client_id,
+            client_secret=client_secret,
+            user_agent=user_agent,
+            username='Glad-Speed8358',
+            password='!Aawwodtr1964'
+        )
         print("Conectado ao Reddit")
         return reddit
     except Exception as e:
         print(e)
-        sys.exit(1) 
+        sys.exit(1)
 
-def extract_posts(reddit_instance: Reddit, subreddit:str, time_filter:str, limit=None):
+def extract_posts(reddit_instance: Reddit, subreddit: str, time_filter: str, limit=None):
     subreddit = reddit_instance.subreddit(subreddit)
     posts = subreddit.top(time_filter=time_filter, limit=limit)
 
@@ -22,27 +28,41 @@ def extract_posts(reddit_instance: Reddit, subreddit:str, time_filter:str, limit
 
     for post in posts:
         post_dict = vars(post)
-        # mapeando os campos do post do reddit, usando os campos definidos em POST_FIELDS
-        post = {key: post_dict[key] for key in POST_FIELDS}
+        # Mapeando os campos do post do Reddit, usando os campos definidos em POST_FIELDS
+        post = {key: post_dict.get(key, None) for key in POST_FIELDS}
         posts_list.append(post)
 
     return posts_list
 
 def transform_data(post_df: pd.DataFrame):
     post_df['created_utc'] = pd.to_datetime(post_df['created_utc'], unit='s')
-    post_df['over_18'] = np.where((post_df['over_18'] == True), True, False)
-    post_df['author'] = post_df['author'].astype(str)
+    
+    if 'over_18' in post_df.columns:
+        post_df['over_18'] = np.where(post_df['over_18'] == True, True, False)
+    
+    if 'author' in post_df.columns:
+        post_df['author'] = post_df['author'].astype(str)
+    
+    if 'edited' in post_df.columns:
+        edited_mode = post_df['edited'].mode()[0] if not post_df['edited'].mode().empty else False
+        post_df['edited'] = np.where(post_df['edited'].isin([True, False]), post_df['edited'], edited_mode).astype(bool)
+    
+    if 'num_comments' in post_df.columns:
+        post_df['num_comments'] = post_df['num_comments'].fillna(0).astype(int)
+    
+    if 'score' in post_df.columns:
+        post_df['score'] = post_df['score'].fillna(0).astype(int)
+    
+    if 'upvote_ratio' in post_df.columns:
+        post_df['upvote_ratio'] = post_df['upvote_ratio'].fillna(0).astype(float)
+    
+    if 'selftext' in post_df.columns:
+        post_df['selftext'] = post_df['selftext'].fillna('').astype(str)
+    
+    if 'title' in post_df.columns:
+        post_df['title'] = post_df['title'].fillna('').astype(str)
 
-    edited_mode = post_df['edited'].mode()
-    post_df['edited'] = np.where((post_df['edited'].isin([True, False]), post_df['edited'], edited_mode).astype(bool))
-
-    post_df['num_comments'] = np.where(post_df['num_comments'].astype(int))
-    post_df['score'] = np.where(post_df['score'].astype(int))
-    post_df['upvote_ratio'] = np.where(post_df['upvote_ratio'].astype(int))
-    post_df['selftext'] = np.where(post_df['selftext'].astype(str))
-    post_df['title'] = np.where(post_df['title'].astype(str))
-
-    return post_df 
+    return post_df
 
 def load_data_to_csv(data: pd.DataFrame, path: str):
     data.to_csv(path, index=False)
--- pipelines/reddit_pipeline.py
+++ pipelines/reddit_pipeline.py
@@ -5,7 +5,7 @@ from etls.reddit_etl import connect_reddit, extract_posts, load_data_to_csv, tra
 
 def reddit_pipeline(file_name: str, subreddit: str, time_filter: str, limit=None):
     # conectando no reddit 
-    instance = connect_reddit(CLIENT_ID, SECRET, 'Airscholar Agent')
+    instance = connect_reddit(CLIENT_ID, SECRET, 'Airscholar Agent by Glad-Speed8358')
     # extracao
     posts = extract_posts(instance, subreddit, time_filter, limit)
     post_df = pd.DataFrame(posts)
